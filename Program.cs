9a015fffd190b899ac49d0a4202d066c3a77fa3e
Максим Мазур <1mazurmaxim12@gmail.com>
2023-04-27 12:13:51 +0300

    реалізував Ninject через module, та клас StandartKernel

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Ninject;
+using HW11_Ninject;
+{
+    var module = new StandardKernel();
+    module.Bind<IDateTimeGenerator>().To<DateTimeGenerator>();
+    module.Bind<IGreetingsGenerator>().To<GreetingsGenerator>();
+    module.Bind<INamesGenerator>().To<NamesGenerator>();
+
+    var finish = module.Get<RandomMessageBuilder>();
+    finish.AddGreeting();
+    finish.AddName();
+    finish.AddDateTime(true);
+    Console.WriteLine(finish.Result());
+}

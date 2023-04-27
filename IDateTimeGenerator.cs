9bfd49cb527e3f889e8583f93b9f1c3adcb0652b
Максим Мазур <1mazurmaxim12@gmail.com>
2023-04-27 12:11:21 +0300

    додав Інтерфейси

---
+++ IDateTimeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW11_Ninject
+{
+    internal interface IDateTimeGenerator
+    {
+        string GetCurrentDateTime();
+        string GetRandomDateTime();
+    }
+}
